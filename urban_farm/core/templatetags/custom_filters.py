2c4558b48cb7d0ce19b3dc867dc92a083d4263e2
Eduardo Bueno <eduardonunesbueno56@gmail.com>
2024-10-16 15:44:46 -0300

    Atualizado fornecedores cnpj e validação

--- urban_farm/core/templatetags/custom_filters.py
+++ urban_farm/core/templatetags/custom_filters.py
@@ -8,3 +8,4 @@ def format_cnpj(cnpj):
     if len(cnpj) == 14:  # Certifica-se de que o CNPJ tem 14 dígitos
         return f"{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:]}"
     return cnpj  # Retorna o CNPJ sem formatação se não tiver 14 dígitos
+
